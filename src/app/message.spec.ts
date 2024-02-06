694a46986cb840030e6aa3b2e828683afebcf221
mbof <mbof@users.noreply.github.com>
2024-02-06 00:01:41 -0800

    Add message encoding and checksum capabilities.

--- src/app/message.spec.ts
+++ src/app/message.spec.ts
@@ -3,29 +3,68 @@ import { Message } from './message';
 const encoder = new TextEncoder();
 
 describe('Message', () => {
-  it('should create an instance from type', () => {
-    expect(new Message({ type: '#CMDOK' })).toBeTruthy();
-  });
+  // Message parsing tests
   it('should parse a CP message with no arguments', () => {
     let m = new Message({ bytes: encoder.encode('#CMDOK\r\n') });
     expect(m.type).toBe('#CMDOK');
+    expect(m.validate()).toBeTrue();
   });
   it('should parse a CP message with a checksum', () => {
-    let m = new Message({ bytes: encoder.encode('#ABC\t1234\r\n') });
-    expect(m.type).toBe('#ABC');
-    expect(m.checksum).toBe('1234');
+    let m = new Message({ bytes: encoder.encode('#CVRRQ\t6E\r\n') });
+    expect(m.type).toBe('#CVRRQ');
+    expect(m.checksum_recv).toBe('6E');
+    expect(m.validate()).toBeTrue();
   });
   it('should parse a CP message with a checksum and arguments', () => {
     let m = new Message({ bytes: encoder.encode(
       '#CEPDT\t0100\t0A\t414D3035374E32FFFFFF\t11\r\n') });
     expect(m.type).toBe('#CEPDT');
-    expect(m.checksum).toBe('11');
+    expect(m.checksum_recv).toBe('11');
     expect(m.args).toEqual(['0100', '0A', '414D3035374E32FFFFFF'])
+    expect(m.validate()).toBeTrue();
   });
   it('should parse a NMEA message', () => {
     let m = new Message({ bytes: encoder.encode('$PMTK001,622,3*36\r\n') });
     expect(m.type).toBe('$PMTK');
-    expect(m.checksum).toBe('36');
+    expect(m.checksum_recv).toBe('36');
     expect(m.args).toEqual(['001', '622', '3'])
+    expect(m.validate()).toBeTrue();
+  });
+
+  // Message checksum validation tests
+  it('should detect a CP message with a wrong checksum', () => {
+    let m = new Message({ bytes: encoder.encode('#CVRRQ\tFF\r\n') });
+    expect(m.validate()).toBeFalse();
+  });
+  it('should detect a CP message with a wrong checksum', () => {
+    let m = new Message({ bytes: encoder.encode(
+      '#CEPDT\t0100\t0A\t414D3035374E32FFFFFF\tAA\r\n') });
+    expect(m.validate()).toBeFalse();
+  });
+  it('should detect a NMEA message with a wrong checksum', () => {
+    let m = new Message({ bytes: encoder.encode('$PMTK001,622,3*72\r\n') });
+    expect(m.validate()).toBeFalse();
+  });
+
+  // Message encoding tests
+  it('should properly encode a unary message without checksum', () => {
+    let m = new Message({type: '#CMDOK'});
+    expect(m.toString()).toBe('#CMDOK\r\n');
+  });
+  it('should properly encode a unary message with checksum', () => {
+    let m = new Message({type: '#CVRRQ'});
+    expect(m.toString()).toBe('#CVRRQ\t6E\r\n');
+  });
+  it('should properly encode a command message with arguments', () => {
+    let m = new Message({type: '#CEPDT', args: ['0100', '0A', '414D3035374E32FFFFFF']});
+    expect(m.toString()).toBe('#CEPDT\t0100\t0A\t414D3035374E32FFFFFF\t11\r\n');
+  });
+  it('should properly encode a NMEA message with 1 argument', () => {
+    let m = new Message({type: '$PMTK', args: ['183']});
+    expect(m.toString()).toBe('$PMTK183*38\r\n');
+  });
+  it('should properly encode a NMEA message with 3 arguments', () => {
+    let m = new Message({type: '$PMTK', args: ['001', '183', '3']});
+    expect(m.toString()).toBe('$PMTK001,183,3*3A\r\n');
   });
 });
--- src/app/message.ts
+++ src/app/message.ts
@@ -1,10 +1,30 @@
+import { reduce } from "rxjs";
 
 const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+const unaryTypes = ["#CMDOK", "#CMDER", "#CMDUN", "#CMDSM", "#CMDSY"];
+
+function checksum(type: string, args: string[]): string | undefined {
+  if (unaryTypes.includes(type)) {
+    return undefined;
+  }
+  if (type[0] == '#') {
+    let check = encoder.encode([type].concat(args).join('\t') + '\t');
+    let s = check.reduce((previous, current) => current == 33 ? previous : previous ^ current);
+    return s.toString(16).toUpperCase().padStart(2, '0');
+  } else if (type[0] == '$') {
+    let check = encoder.encode([type.slice(1)] + args.join(','));
+    let s = check.reduce((previous, current) => current == 33 ? previous : previous ^ current);
+    return s.toString(16).toUpperCase().padStart(2, '0');
+  }
+  return undefined;
+}
 
 export class Message {
-  readonly type: string | undefined;
+  readonly type: string;
   readonly args: string[];
-  readonly checksum: string | undefined;
+  readonly checksum_recv: string | undefined;
+  readonly checksum_calc: string | undefined;
 
   constructor({ bytes, type, args }:
     {
@@ -12,9 +32,13 @@ export class Message {
       type?: string | undefined;
       args?: Array<string> | undefined;
     }) {
-    this.type = type;
     this.args = args || [];
     if (!bytes) {
+      if (!type) {
+        throw new Error('Missing message type');
+      }
+      this.type = type;
+      this.checksum_calc = checksum(type, this.args);
       return;
     }
     let text = decoder.decode(bytes);
@@ -23,7 +47,7 @@ export class Message {
       let components = text.replace(/[\r\n]*$/, '').split('\t');
       this.type = components[0];
       if (components.length > 1) {
-        this.checksum = components.at(-1);
+        this.checksum_recv = components.at(-1);
         // TODO: check the checksum.
       }
       if (components.length > 2) {
@@ -33,10 +57,31 @@ export class Message {
       // NMEA sentence
       this.type = text.slice(0, 5);
       let components = text.slice(5).replace(/[\r\n]*$/, '').split('*');
-      this.checksum = components[1];
+      this.checksum_recv = components[1];
       this.args = components[0].split(',');
     } else {
       throw new Error(`Invalid message ${text}`);
     }
+    this.checksum_calc = checksum(this.type, this.args);
+  }
+
+  toString(): string {
+    if (!this.checksum_calc) {
+      return this.type + '\r\n';
+    }
+    let checksum = this.checksum_recv || this.checksum_calc;
+    if (this.type[0] == '#') {
+      return [this.type].
+        concat(this.args).
+        concat([checksum]).
+        join('\t') + '\r\n';
+    } else if (this.type[0] == '$') {
+      return [this.type] + this.args.join(',') + `*${checksum}\r\n`;
+    }
+    throw new Error(`Unknown message type ${this.type}`);
+  }
+
+  validate(): boolean {
+    return !this.checksum_calc || !this.checksum_recv || this.checksum_calc == this.checksum_recv;
   }
 }
