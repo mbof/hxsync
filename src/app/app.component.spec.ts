b3215860cfce2fb6ad043fe7ec42e96151c4434b
mbof <mbof@users.noreply.github.com>
2024-02-13 23:44:13 -0800

    Fix test bug.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hx890');
+    expect(compiled.querySelector('h1')?.textContent).toContain('HxSync');
   });
 });
