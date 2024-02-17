9020d0a37c8b3338f9e2384e5fe56c2629103309
mbof <mbof@users.noreply.github.com>
2024-02-17 02:23:21 -0800

    Waypoint editing (#11)
    * Add table rendering for waypoints.
    
    * Fix waypoint decoding bug. Add link to view waypoint on map. Change and apply formatting preference for 80-char width.
    
    * Add waypoint draft management and waypoint encoding.
    
    * Add waypoint editor dialog.
    
    * Add capability to add waypoints.
    
    * Add form validation. Make enter submit changes.
    
    * Show more debug info in waypoint table.
    Start waypoints IDs at 1 instead of 0.
    Pad labels with spaces, as the device does
    
    * Fix bug with lat/lon parsing. Update test to match waypoint data created on device.

--- src/app/waypoint.spec.ts
+++ src/app/waypoint.spec.ts
@@ -1,3 +1,232 @@
-import { Waypoint } from './waypoint';
+import { hexarr, unhex } from './message';
+import {
+  DraftWaypoints,
+  Waypoint,
+  WaypointData,
+  parseAndCheckWaypointData,
+  parseLat,
+  parseLon,
+  waypointFromConfig
+} from './waypoint';
 
-// TOOD: add tests
+const testWpData: WaypointData = {
+  id: 11,
+  name: 'Test wp',
+  lat_deg: 45,
+  lat_min: 67890,
+  lat_dir: 'N',
+  lon_deg: 123,
+  lon_min: 35670,
+  lon_dir: 'W',
+  address: 0x1234
+};
+// prettier-ignore
+const testEncodedWaypoint = new Uint8Array([
+  // Four padding bytes
+  0xff, 0xff, 0xff, 0xff,
+  // Latitude
+  0xf0, 0x45, 0x06, 0x78, 0x90, 'N'.charCodeAt(0),
+  // Longitude
+  0x01, 0x23, 0x03, 0x56, 0x70, 'W'.charCodeAt(0),
+  // Name
+  0x54, 0x65, 0x73, 0x74, 0x20, 0x77, 0x70, 0x20,
+  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00,
+  // ID
+  0x0B
+]);
+
+const testEncodedWaypoint2 = unhex(
+  'FFFFFFFFF0332072004E0118194170574176616C6F6E2020202020202020000A'
+);
+
+describe('Waypoint', () => {
+  it('should pack a waypoint', () => {
+    const wp = new Waypoint(testWpData);
+    const dest = new Uint8Array(32);
+    dest.fill(0xaa, 0, 32);
+    wp.fillConfig(dest, 0x1234);
+    expect(dest).toEqual(testEncodedWaypoint);
+  });
+  it('should return readable lat / lon', () => {
+    const wp = new Waypoint(testWpData);
+    expect(wp.getLat()).toEqual('45N06.7890');
+    expect(wp.getLon()).toEqual('123W03.5670');
+    expect(wp.getMapLink()).toEqual(
+      'https://www.google.com/maps/place/45.11315,-123.05945'
+    );
+  });
+});
+
+describe('waypointFromConfig', () => {
+  it('should decode a waypoint', () => {
+    const wp = waypointFromConfig(testEncodedWaypoint, 0x1234);
+    expect(wp).toBeTruthy();
+    expect(wp!.wp).toEqual(testWpData);
+  });
+});
+
+describe('parseLat', () => {
+  it('should parse a latitude', () => {
+    const lat = parseLat('45N06.7890');
+    expect(lat).toEqual({
+      lat_deg: 45,
+      lat_dir: 'N',
+      lat_min: 67890
+    });
+  });
+});
+
+describe('parseLon', () => {
+  it('should parse a longitude', () => {
+    const lon = parseLon('123W03.5670');
+    expect(lon).toEqual({
+      lon_deg: 123,
+      lon_dir: 'W',
+      lon_min: 35670
+    });
+  });
+});
+
+describe('parseAndCheckWaypointData', () => {
+  it('should accept valid data', () => {
+    const result = parseAndCheckWaypointData({
+      name: 'Test waypoint',
+      lat: '45N06.7890',
+      lon: '123W03.5670'
+    });
+    expect(result.lat).toEqual({
+      lat_deg: 45,
+      lat_dir: 'N',
+      lat_min: 67890
+    });
+    expect(result.lon).toEqual({
+      lon_deg: 123,
+      lon_dir: 'W',
+      lon_min: 35670
+    });
+  });
+  it('should reject long names', () => {
+    expect(() =>
+      parseAndCheckWaypointData({
+        name: 'Test waypoint but too long',
+        lat: '45N06.7890',
+        lon: '123W03.5670'
+      })
+    ).toThrow();
+  });
+  it('should reject non-ASCII names', () => {
+    expect(() =>
+      parseAndCheckWaypointData({
+        name: 'Test wäypoint',
+        lat: '45N06.7890',
+        lon: '123W03.5670'
+      })
+    ).toThrow();
+  });
+  it('should reject non-parseable lat / lon', () => {
+    expect(() =>
+      parseAndCheckWaypointData({
+        name: 'Test waypoint',
+        lat: '45Z06.7890',
+        lon: '123W03.5670'
+      })
+    ).toThrow();
+  });
+});
+
+function getSampleWaypointArray() {
+  const wp = waypointFromConfig(testEncodedWaypoint, 0x1234)!;
+  const wp2 = new Waypoint({ ...wp.wp, id: 0, address: 0x1254 });
+  const wp3 = new Waypoint({ ...wp.wp, id: 1, address: 0x1274 });
+  const wp4 = new Waypoint({ ...wp.wp, id: 2, address: 0x1294 });
+  return [wp, wp2, wp3, wp4];
+}
+
+describe('DraftWaypoints', () => {
+  it('should create a set of draft waypoints', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 100);
+    expect(draft).toBeTruthy();
+    expect(draft.waypoints).toEqual(wpArr);
+    expect(draft.waypoints).not.toBe(wpArr);
+    expect(draft.dirty).toBeFalse();
+  });
+  it('should delete waypoints', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 100);
+    draft.deleteWaypoint(wpArr[2]);
+    expect(draft.waypoints).toEqual([wpArr[0], wpArr[1], wpArr[3]]);
+    expect(draft.dirty).toBeTrue();
+  });
+  it('should edit waypoints', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 100);
+    draft.editWaypoint(wpArr[2], 'New name', '10S34.5678', '100E45.6789');
+    expect(draft.waypoints[0]).toBe(wpArr[0]);
+    expect(draft.waypoints[1]).toBe(wpArr[1]);
+    expect(draft.waypoints[3]).toBe(wpArr[3]);
+    expect(draft.waypoints[2].wp.name).toBe('New name');
+    expect(draft.waypoints[2].wp.lat_deg).toBe(10);
+    expect(draft.waypoints[2].wp.lat_dir).toBe('S');
+    expect(draft.waypoints[2].wp.lat_min).toBe(345678);
+    expect(draft.waypoints[2].wp.lon_deg).toBe(100);
+    expect(draft.waypoints[2].wp.lon_dir).toBe('E');
+    expect(draft.waypoints[2].wp.lon_min).toBe(456789);
+    expect(draft.dirty).toBeTrue();
+  });
+  it('should add waypoints', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 100);
+    expect(draft.waypoints.length == 4);
+    draft.addWaypoint({
+      name: 'Avalon',
+      lat: '33N20.720',
+      lon: '118W19.417'
+    });
+    const dest = new Uint8Array(32);
+    expect(draft.waypoints.length == 5);
+    const addedWp = draft.waypoints[4];
+    expect(addedWp.wp.name).toBe('Avalon');
+    expect(addedWp.wp.id).toBe(3);
+    addedWp.wp.address = 0x1234;
+    addedWp.wp.id = 10;
+    addedWp.fillConfig(dest, 0x1234);
+    expect(dest).toEqual(testEncodedWaypoint2);
+    expect(addedWp.wp.lat_deg).toBe(33);
+    expect(addedWp.wp.lat_dir).toBe('N');
+    expect(addedWp.wp.lat_min).toBe(207200);
+    expect(addedWp.wp.lon_deg).toBe(118);
+    expect(addedWp.wp.lon_dir).toBe('W');
+    expect(addedWp.wp.lon_min).toBe(194170);
+    expect(draft.dirty).toBeTrue();
+  });
+  it('should reject waypoints when full', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 4);
+    expect(() =>
+      draft.addWaypoint({
+        name: 'New waypoint',
+        lat: '10S34.5678',
+        lon: '100E45.6789'
+      })
+    ).toThrow();
+    expect(draft.dirty).toBeFalse();
+  });
+  it('should encode waypoints', () => {
+    const wpArr = getSampleWaypointArray();
+    const draft = new DraftWaypoints(wpArr, 6);
+    draft.addWaypoint({
+      name: 'New waypoint',
+      lat: '10S34.5678',
+      lon: '100E45.6789'
+    });
+    expect(hexarr(draft.getBinaryData(0x1234))).toBe(
+      'FFFFFFFFF010345678530100456789454E657720776179706F696E7420200003' +
+        'FFFFFFFFF0450678904E0123035670575465737420777020202020202020000B' +
+        'FFFFFFFFF0450678904E01230356705754657374207770202020202020200000' +
+        'FFFFFFFFF0450678904E01230356705754657374207770202020202020200001' +
+        'FFFFFFFFF0450678904E01230356705754657374207770202020202020200002' +
+        'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
+    );
+  });
+});
--- src/app/waypoint.ts
+++ src/app/waypoint.ts
@@ -1,19 +1,53 @@
-import { hex, hexarr, unhex } from './message';
+import { hex, hexarr, unhex, unhexInto } from './message';
+
+export type WaypointData = {
+  id: number;
+  name: string;
+  lat_deg: number;
+  lat_dir: string;
+  lat_min: number;
+  lon_deg: number;
+  lon_dir: string;
+  lon_min: number;
+  address?: number;
+};
+
+export function parseLat(
+  lat: string
+): { lat_deg: number; lat_dir: string; lat_min: number } | undefined {
+  const parsed_lat = lat.toUpperCase().match(/(\d+)(N|S)(\d+)\.(\d+)/);
+  if (!parsed_lat) {
+    return undefined;
+  }
+  let [_unused, lat_deg_str, lat_dir, lat_min_str, lat_min_tenthousandths] =
+    parsed_lat;
+  lat_min_tenthousandths = lat_min_tenthousandths.slice(0, 4).padEnd(4, '0');
+  const lat_deg = Number.parseInt(lat_deg_str, 10);
+  const lat_min =
+    Number.parseInt(lat_min_str, 10) * 10000 +
+    Number.parseInt(lat_min_tenthousandths);
+  return { lat_deg: lat_deg, lat_dir: lat_dir, lat_min: lat_min };
+}
+
+export function parseLon(
+  lon: string
+): { lon_deg: number; lon_dir: string; lon_min: number } | undefined {
+  const parsed_lon = lon.toUpperCase().match(/(\d+)(E|W)(\d+)\.(\d+)/);
+  if (!parsed_lon) {
+    return undefined;
+  }
+  let [_unused, lon_deg_str, lon_dir, lon_min_str, lon_min_tenthousandths] =
+    parsed_lon;
+  lon_min_tenthousandths = lon_min_tenthousandths.slice(0, 4).padEnd(4, '0');
+  const lon_deg = Number.parseInt(lon_deg_str, 10);
+  const lon_min =
+    Number.parseInt(lon_min_str, 10) * 10000 +
+    Number.parseInt(lon_min_tenthousandths);
+  return { lon_deg: lon_deg, lon_dir: lon_dir, lon_min: lon_min };
+}
 
 export class Waypoint {
-  constructor(
-    public wp: {
-      id: number;
-      name: string;
-      lat_deg: number;
-      lat_dir: string;
-      lat_min: number;
-      lon_deg: number;
-      lon_dir: string;
-      lon_min: number;
-      address?: number;
-    }
-  ) {}
+  constructor(public wp: WaypointData) {}
   toString(): string {
     let wpStr = `${this.wp.id}, ${this.wp.name}, ${this.wp.lat_deg}${this.wp.lat_dir}${this.wp.lat_min}, ${this.wp.lon_deg}${this.wp.lon_dir}${this.wp.lon_min}`;
     if (this.wp.address) {
@@ -22,16 +56,87 @@ export class Waypoint {
       return wpStr;
     }
   }
+  getLat() {
+    const lat_min_int = Math.floor(this.wp.lat_min / 10000)
+      .toString(10)
+      .padStart(2, '0');
+    const lat_min_flt = (this.wp.lat_min % 10000).toString(10).padStart(4, '0');
+    return `${this.wp.lat_deg}${this.wp.lat_dir}${lat_min_int}.${lat_min_flt}`;
+  }
+  getLon() {
+    const lon_min_int = Math.floor(this.wp.lon_min / 10000)
+      .toString(10)
+      .padStart(2, '0');
+    const lon_min_flt = (this.wp.lon_min % 10000).toString(10).padStart(4, '0');
+    return `${this.wp.lon_deg}${this.wp.lon_dir}${lon_min_int}.${lon_min_flt}`;
+  }
+  getMapLink() {
+    const lat_flt =
+      (this.wp.lat_dir == 'N' ? 1 : -1) *
+      (this.wp.lat_deg + this.wp.lat_min / 10000 / 60);
+    const lon_flt =
+      (this.wp.lon_dir == 'E' ? 1 : -1) *
+      (this.wp.lon_deg + this.wp.lon_min / 10000 / 60);
+    return `https://www.google.com/maps/place/${lat_flt},${lon_flt}`;
+  }
+  fillConfig(dest: Uint8Array, destAddressBase: number): void {
+    if (!this.wp.address) {
+      throw new Error('The waypoint must have an address');
+    }
+    const destOffset = this.wp.address - destAddressBase;
+    if (destOffset < 0 || destOffset > dest.length - 32) {
+      throw new Error(
+        `Waypoint address ${this.wp.address} (offset ${destOffset}) is out of bounds`
+      );
+    }
+    let name = this.wp.name;
+    if (name.length >= 15) {
+      console.log(`Waypoint name too long ${name}, truncating`);
+      name = name.substring(0, 14);
+    }
+    name = name.padEnd(14, ' ');
+    const destWaypoint = dest.subarray(destOffset, destOffset + 32);
+    destWaypoint[31] = this.wp.id;
+    const destName = destWaypoint.subarray(16, 31);
+    const { written: nameByteLength } = encoder.encodeInto(name, destName);
+    destName.fill(0, nameByteLength, 15);
+    destWaypoint.fill(255, 0, 4);
+    const destLat = destWaypoint.subarray(4, 9);
+    let lat_hex = 'F';
+    lat_hex += this.wp.lat_deg.toString(10).padStart(3, '0');
+    lat_hex += this.wp.lat_min.toString(10).padStart(6, '0');
+    if (lat_hex.length != 10) {
+      throw new Error(`Encoded waypoint lat too long ${lat_hex}`);
+    }
+    unhexInto(lat_hex, destLat);
+    destWaypoint[9] = this.wp.lat_dir.charCodeAt(0);
+    const destLon = destWaypoint.subarray(10, 15);
+    let lon_hex = this.wp.lon_deg.toString(10).padStart(4, '0');
+    lon_hex += this.wp.lon_min.toString(10).padStart(6, '0');
+    if (lat_hex.length != 10) {
+      throw new Error(`Encoded waypoint lon too long ${lon_hex}`);
+    }
+    unhexInto(lon_hex, destLon);
+    destWaypoint[15] = this.wp.lon_dir.charCodeAt(0);
+  }
 }
 
 const decoder = new TextDecoder();
+const encoder = new TextEncoder();
 
-export function waypointFromConfig(wpData: Uint8Array, address?: number): Waypoint | undefined {
+export function waypointFromConfig(
+  wpData: Uint8Array,
+  address?: number
+): Waypoint | undefined {
   let id = wpData[31];
   if (id == 255) {
     return;
   }
-  for (var lastChar = 30; lastChar >= 16 && [0, 255, 32].includes(wpData[lastChar]); lastChar -= 1);
+  for (
+    var lastChar = 30;
+    lastChar >= 16 && [0, 255, 32].includes(wpData[lastChar]);
+    lastChar -= 1
+  );
   let name = decoder.decode(wpData.slice(16, lastChar + 1));
   let lat_str = hexarr(wpData.slice(4, 9)).slice(1);
   let lat_deg = Number(lat_str.slice(0, 3));
@@ -39,7 +144,7 @@ export function waypointFromConfig(wpData: Uint8Array, address?: number): Waypoi
   let lat_dir = decoder.decode(wpData.slice(9, 10));
   let lon_str = hexarr(wpData.slice(10, 15));
   let lon_deg = Number(lon_str.slice(0, 4));
-  let lon_min = Number(lat_str.slice(4, 10));
+  let lon_min = Number(lon_str.slice(4, 10));
   let lon_dir = decoder.decode(wpData.slice(15, 16));
 
   return new Waypoint({
@@ -54,3 +159,125 @@ export function waypointFromConfig(wpData: Uint8Array, address?: number): Waypoi
     address: address
   });
 }
+
+export function parseAndCheckWaypointData({ name, lat, lon }: WpFormData) {
+  if (name.length == 0) {
+    throw new Error(`No name was provided`);
+  }
+  if (name.length >= 15) {
+    throw new Error(`Waypoint name too long ${name}`);
+  }
+  if (!/^[\x20-\x7F]+$/.test(name)) {
+    throw new Error(`Name contains bad characters ${name}`);
+  }
+  const parsedLat = parseLat(lat);
+  const parsedLon = parseLon(lon);
+  if (!parsedLat || !parsedLon) {
+    throw new Error(`Unparseable position ${lat} ${lon}`);
+  }
+  return { lat: parsedLat, lon: parsedLon };
+}
+
+export const WAYPOINTS_BYTE_SIZE = 32;
+
+export class DraftWaypoints {
+  waypoints: Waypoint[];
+  dirty = false;
+  constructor(
+    waypoints: Waypoint[],
+    private maxWaypoints: number,
+    private updateCallback?: (() => void) | undefined
+  ) {
+    this.waypoints = waypoints.slice();
+  }
+
+  private maybeUpdateCallback() {
+    this.dirty = true;
+    if (this.updateCallback) {
+      this.updateCallback();
+    }
+  }
+
+  deleteWaypoint(wpToDelete: Waypoint): void {
+    const index = this.waypoints.findIndex(
+      (wp) => wp.wp.address == wpToDelete.wp.address
+    );
+    this.waypoints.splice(index, 1);
+    this.maybeUpdateCallback();
+  }
+
+  editWaypoint(
+    wpToChange: Waypoint,
+    name: string,
+    lat_str: string,
+    lon_str: string
+  ): void {
+    if (name.length > 15) {
+      throw new Error(`Waypoint name too long ${name}`);
+    }
+    const lat = parseLat(lat_str);
+    const lon = parseLon(lon_str);
+    if (!lat || !lon) {
+      throw new Error(`Unparseable position ${lat_str} ${lon_str}`);
+    }
+    const index = this.waypoints.findIndex(
+      (wp) => wp.wp.address == wpToChange.wp.address
+    );
+    if (index == -1) {
+      throw new Error(`Could not find waypoint at ${wpToChange.wp.address}`);
+    }
+    this.waypoints[index] = new Waypoint({
+      ...this.waypoints[index].wp,
+      ...lat,
+      ...lon,
+      name: name
+    });
+    this.maybeUpdateCallback();
+  }
+
+  addWaypoint(wpFormData: WpFormData): void {
+    if (this.waypoints.length >= this.maxWaypoints) {
+      throw new Error('No more room for waypoints');
+    }
+    const { lat, lon } = parseAndCheckWaypointData(wpFormData);
+    let nextId: number;
+    // Find the first available ID
+    for (nextId = 1; nextId < 255; nextId += 1) {
+      if (!this.waypoints.find((wp) => wp.wp.id == nextId)) {
+        break;
+      }
+    }
+    this.waypoints.push(
+      new Waypoint({
+        ...lat,
+        ...lon,
+        name: wpFormData.name,
+        id: nextId
+      })
+    );
+    this.maybeUpdateCallback();
+  }
+
+  getBinaryData(wpBaseAddress: number): Uint8Array {
+    // Sort waypoints alphabetically
+    this.waypoints.sort((wpA, wpB) => wpA.wp.name.localeCompare(wpB.wp.name));
+    // Reassign addresses from the top
+    let address = wpBaseAddress;
+    for (let wp of this.waypoints) {
+      wp.wp.address = address;
+      address += WAYPOINTS_BYTE_SIZE;
+    }
+    // Prepare all waypoint data
+    const wpData = new Uint8Array(WAYPOINTS_BYTE_SIZE * this.maxWaypoints);
+    wpData.fill(255);
+    for (const wp of this.waypoints) {
+      wp.fillConfig(wpData, wpBaseAddress);
+    }
+    return wpData;
+  }
+}
+export type WpFormData = {
+  name: string;
+  lat: string;
+  lon: string;
+};
