09268133e929494d173e93d6d5ef3cc128e5053f
mbof <mbof@users.noreply.github.com>
2024-05-30 00:35:57 -0700

    Add functionality that writes config to device.

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -1,6 +1,8 @@
 import { HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DevicemgrService } from '../devicemgr.service';
+import { DeviceTaskState } from '../config-session';
 
 @Component({
   selector: 'app-share',
@@ -11,8 +13,44 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShareComponent {
   yaml: string = '';
-  constructor(private route: ActivatedRoute) {}
+  status: string = '';
+  state: DeviceTaskState = 'idle';
+  progress: number = 0;
+  constructor(
+    private deviceMgr: DevicemgrService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit() {
     this.route.fragment.subscribe((f) => (this.yaml = f || ''));
+    this.deviceMgr.configSession.deviceTaskState$.subscribe((state) => {
+      this.state = state;
+    });
+    this.deviceMgr.configSession.progress$.subscribe((progress) => {
+      this.progress = progress;
+    });
+  }
+  async apply() {
+    try {
+      this.status = '';
+      if (this.deviceMgr.getConnectionState() != 'usb-connected') {
+        await this.deviceMgr.connectUsb();
+      }
+      await this.deviceMgr.configSession.startYaml();
+      await this.deviceMgr.configSession.saveYaml(this.yaml);
+    } catch (e) {
+      this.status = e!.toString();
+    }
+  }
+  getStatus() {
+    if (this.status) {
+      return this.status;
+    }
+    if (this.state == 'yaml-read') {
+      return `Reading from device (${Math.floor(this.progress * 100)}%)`;
+    }
+    if (this.state == 'yaml-save') {
+      return `Writing to device (${Math.floor(this.progress * 100)}%)`;
+    }
+    return '';
   }
 }
