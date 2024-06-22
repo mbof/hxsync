a6434b87f775d6c260abbecbce818433001a0072
mbof <mbof@users.noreply.github.com>
2024-06-22 16:39:12 -0700

    Remove hash location. This seems to cause redirects that prevent search indexing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,6 @@
 import { Routes } from '@angular/router';
-import { ShareComponent } from './share/share.component';
 import { DeviceComponent } from './device/device.component';
 
 export const routes: Routes = [
-  { path: '', component: DeviceComponent, pathMatch: 'full' },
-  {
-    matcher: (url) => {
-      if (url.length > 0 && url[0].path.match(/^share/)) {
-        return {
-          consumed: url
-        };
-      }
-      return null;
-    },
-    component: ShareComponent
-  }
+  { path: '', component: DeviceComponent, pathMatch: 'full' }
 ];
