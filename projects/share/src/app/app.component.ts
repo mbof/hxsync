b52c94092204f18b2ee5813c4704e7003726853b
mbof <mbof@users.noreply.github.com>
2025-03-11 00:28:35 -0700

    Removed unused import

--- projects/share/src/app/app.component.ts
+++ projects/share/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ShareComponent } from '../../../../src/app/share/share.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, ShareComponent],
+    imports: [ShareComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
