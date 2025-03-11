6b0ae78c6080ffcc6e62d50c2c03b312b7131b50
mbof <mbof@users.noreply.github.com>
2025-03-11 00:27:51 -0700

    Remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DeviceComponent } from './device/device.component';
 import { version, buildDate, commitHash } from '../environments/version';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, DeviceComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
